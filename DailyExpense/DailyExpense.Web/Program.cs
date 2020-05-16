4ebeb0506ac9aa8188beb464e9be8433005430fb
nasir <nunnasir@gmail.com>
2020-05-16 20:51:10 +0600

    Add Dockerfile

--- DailyExpense/DailyExpense.Web/Program.cs
+++ DailyExpense/DailyExpense.Web/Program.cs
@@ -23,6 +23,7 @@ namespace DailyExpense.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
